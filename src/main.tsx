3aee0f8412152051dfaba014a680017849550eb7
Kazi-Mayesha-Mehzabin <mayesha.mehzabin8@gmail.com>
2026-08-24 18:01:30 +0600

    init3

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // Or HashRouter
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* 👇 ADD basename="/drshidujaman" EXACTLY HERE */}
+    <BrowserRouter basename="/drshidujaman"> 
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>
 )
